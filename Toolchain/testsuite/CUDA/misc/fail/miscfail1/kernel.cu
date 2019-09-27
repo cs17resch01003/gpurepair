9238405e3720e192d074b25e74d139416e267c79
Gautam Muduganti <cs17resch01003@iith.ac.in>
2019-09-27 19:04:54 +0530

    Removing kernels with Bugle and Clang errors.

--- Toolchain/testsuite/CUDA/misc/fail/miscfail1/kernel.cu
+++
@@ -1,9 +0,0 @@
-//xfail:CLANG_ERROR
-//--blockDim=4 --gridDim=2 --no-inline
-//kernel.cu:8:3:[\s]+error: use of undeclared identifier 'foo'
-
-#include <cuda.h>
-
-__global__ void k() {
-  foo();
-}
--- Toolchain/testsuite/CUDA/return_val/longlong/kernel.cu
+++
@@ -1,9 +0,0 @@
-//xfail:BUGLE_ERROR
-//--blockDim=1024 --gridDim=1 --no-inline
-//error: Type of __return_val function does not match return type
-
-__device__ long long multiplyByTwo(int i)
-{
-    __ensures(__return_val_int() == i * 2);
-    return i * 2;
-}
