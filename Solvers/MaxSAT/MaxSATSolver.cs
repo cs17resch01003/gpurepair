a573375f586fe8655df958bbdeb8e4a3c8a50405
Gautam Muduganti <cs17resch01003@iith.ac.in>
2019-10-02 01:01:40 +0530

    Using inbuilt MaxSAT implementation.

--- Solvers/MaxSAT/MaxSATSolver.cs
+++ Solvers/MaxSAT/MaxSATSolver.cs
@@ -10,12 +10,12 @@ namespace GPURepair.Solvers
         /// <summary>
         /// The hard clauses.
         /// </summary>
-        public List<Clause> HardClauses { get; set; }
+        private List<Clause> hard_clauses { get; set; }
 
         /// <summary>
         /// The soft clauses.
         /// </summary>
-        public List<Clause> SoftClauses { get; set; }
+        private List<Clause> soft_clauses { get; set; }
 
         /// <summary>
         /// The constructor.
@@ -24,8 +24,8 @@ namespace GPURepair.Solvers
         /// <param name="soft_clauses">The soft clauses.</param>
         public MaxSATSolver(List<Clause> hard_clauses, List<Clause> soft_clauses)
         {
-            HardClauses = hard_clauses;
-            SoftClauses = soft_clauses;
+            this.hard_clauses = hard_clauses;
+            this.soft_clauses = soft_clauses;
         }
 
         /// <summary>
@@ -34,64 +34,34 @@ namespace GPURepair.Solvers
         /// <returns>The solution.</returns>
         public Dictionary<string, bool> Solve()
         {
-            int relaxed = 0;
-            int limit = SoftClauses.Count;
-
-            while (relaxed <= limit)
-            {
-                Dictionary<string, bool> solution = Solve(relaxed);
-                if (solution != null)
-                    return solution;
-
-                relaxed += 1;
-            }
-
-            throw new SatisfiabilityError();
-        }
-
-        /// <summary>
-        /// Solves the clauses and returns a solution.
-        /// </summary>
-        /// <returns>The solution.</returns>
-        public Dictionary<string, bool> Solve(int relaxed)
-        {
             // Use Z3 and figure out the variable assignments
             using (Context context = new Context())
             {
                 Dictionary<string, Z3Variable> variables = GetVariables(context);
 
-                List<BoolExpr> hard_clauses = GenerateClauses(context, HardClauses, variables);
-                List<BoolExpr> soft_clauses = GenerateClauses(context, SoftClauses, variables);
+                List<BoolExpr> hard_clauses = GenerateClauses(context, this.hard_clauses, variables);
+                List<BoolExpr> soft_clauses = GenerateClauses(context, this.soft_clauses, variables);
 
-                List<BoolExpr> final_clauses;
-                if (relaxed == 0)
+                Optimize optimize = context.MkOptimize();
+                optimize.Assert(hard_clauses);
+                foreach (BoolExpr clause in soft_clauses)
+                    optimize.AssertSoft(clause, 1, "group");
+
+                Status status = optimize.Check();
+                if (status == Status.SATISFIABLE)
                 {
-                    final_clauses = hard_clauses.Union(soft_clauses).ToList();
-                }
-                else
-                {
-                    Dictionary<string, Z3Variable> relaxation_variables = GetRelaxationVariables(context, soft_clauses.Count);
-                    soft_clauses = AddRelaxationVariables(context, soft_clauses, relaxation_variables);
+                    Dictionary<string, bool> assignments = new Dictionary<string, bool>();
+                    foreach (string variable in variables.Keys)
+                    {
+                        Expr expr = optimize.Model.Evaluate(variables[variable].Positive);
+                        if (expr.BoolValue != Z3_lbool.Z3_L_UNDEF)
+                            assignments.Add(variable, expr.BoolValue == Z3_lbool.Z3_L_TRUE ? true : false);
+                    }
 
-                    List<BoolExpr> relaxation_clauses = GenerateConstraints(context, relaxation_variables, relaxed);
-                    final_clauses = hard_clauses.Union(soft_clauses).Union(relaxation_clauses).ToList();
-                }
-
-                Model model = SAT(final_clauses, context);
-                if (model == null)
-                    return null;
-
-                Dictionary<string, bool> assignments = new Dictionary<string, bool>();
-                foreach (string variable in variables.Keys)
-                {
-                    BoolExpr key = variables[variable].Positive;
-                    if (model.Evaluate(key).BoolValue == Z3_lbool.Z3_L_TRUE)
-                        assignments.Add(variable, true);
-                    else
-                        assignments.Add(variable, false);
+                    return assignments;
                 }
 
-                return assignments;
+                throw new SatisfiabilityError();
             }
         }
 
@@ -104,7 +74,7 @@ namespace GPURepair.Solvers
         {
             Dictionary<string, Z3Variable> variables = new Dictionary<string, Z3Variable>();
 
-            IEnumerable<string> variableNames = HardClauses.Union(SoftClauses)
+            IEnumerable<string> variableNames = hard_clauses.Union(soft_clauses)
                 .SelectMany(x => x.Literals).Select(x => x.Variable).Distinct();
             foreach (string variableName in variableNames)
                 variables.Add(variableName, new Z3Variable(context, variableName));
@@ -136,132 +106,6 @@ namespace GPURepair.Solvers
             }
 
             return clauses;
-        }
-
-        /// <summary>
-        /// Generates relaxation variables for MaxSAT.
-        /// </summary>
-        /// <param name="context">The Z3 context.</param>
-        /// <param name="count">The number of soft clauses.</param>
-        /// <returns>The relaxation variables.</returns>
-        private Dictionary<string, Z3Variable> GetRelaxationVariables(Context context, int count)
-        {
-            Dictionary<string, Z3Variable> relaxation_variables = new Dictionary<string, Z3Variable>();
-            for (int i = 0; i < count; i++)
-            {
-                string variableName = string.Format("r_{0}", i);
-                relaxation_variables.Add(variableName, new Z3Variable(context, variableName));
-            }
-
-            return relaxation_variables;
-        }
-
-        /// <summary>
-        /// Adds the relaxation variable to the soft clauses.
-        /// </summary>
-        /// <param name="context">The Z3 context.</param>
-        /// <param name="soft_clauses">The soft clauses.</param>
-        /// <param name="relaxation_variables">The relaxation variables.</param>
-        /// <returns>The Z3 soft clauses.</returns>
-        private List<BoolExpr> AddRelaxationVariables(Context context, List<BoolExpr> soft_clauses,
-             Dictionary<string, Z3Variable> relaxation_variables)
-        {
-            List<BoolExpr> relaxed_clauses = new List<BoolExpr>();
-
-            for (int i = 0; i < soft_clauses.Count; i++)
-            {
-                BoolExpr clause = soft_clauses[i];
-                Z3Variable variable = relaxation_variables[string.Format("r_{0}", i)];
-
-                relaxed_clauses.Add(context.MkOr(clause, variable.Positive));
-            }
-
-            return relaxed_clauses;
-        }
-
-        /// <summary>
-        /// Generates constraints to restrict the relaxation variables.
-        /// Implements the Sequential Counter Encoding scheme - http://www.cs.toronto.edu/~fbacchus/csc2512/at_most_k.pdf.
-        /// </summary>
-        /// <param name="context">The Z3 context.</param>
-        /// <param name="relaxation_variables">The relaxation variables.</param>
-        /// <param name="relaxed">The relaxation limit.</param>
-        /// <returns>The constraints to restrict the relaxation variables.</returns>
-        private List<BoolExpr> GenerateConstraints(Context context,
-            Dictionary<string, Z3Variable> relaxation_variables, int relaxed)
-        {
-            List<BoolExpr> inputs = relaxation_variables.Select(x => x.Value.Negative).ToList();
-            Z3Variable[,] registers = CreateRegisterVariables(context, inputs.Count, relaxed);
-
-            int n = inputs.Count;
-            int k = relaxed;
-
-            List<BoolExpr> clauses = new List<BoolExpr>();
-
-            // ensures that if the variable is true,
-            // the first bit of the register is true
-            for (int i = 0; i < n - 1; i++)
-                clauses.Add(context.MkOr(inputs[i], registers[i, 0].Positive));
-
-            // only the first bit of the first register can be true
-            for (int j = 1; j < k; j++)
-                clauses.Add(context.MkOr(registers[0, j].Negative));
-
-            // contain the value of the previous register
-            // plus the current variable
-            for (int i = 1; i < n - 1; i++)
-                for (int j = 0; j < k; j++)
-                    clauses.Add(context.MkOr(registers[i - 1, j].Negative, registers[i, j].Positive));
-
-            // contain the value of the previous register
-            // plus the current variable
-            for (int i = 1; i < n - 1; i++)
-                for (int j = 1; j < k; j++)
-                    clauses.Add(context.MkOr(inputs[i], registers[i - 1, j - 1].Negative, registers[i, j].Positive));
-
-            // asserts that there is no overflow
-            for (int i = 1; i < n; i++)
-                clauses.Add(context.MkOr(inputs[i], registers[i - 1, relaxed - 1].Negative));
-
-            return clauses;
-        }
-
-        /// <summary>
-        /// Creates the register variables based on the number of variables and the relaxation limit.
-        /// </summary>
-        /// <param name="context">The Z3 context.</param>
-        /// <param name="variables">The number of variables.</param>
-        /// <param name="relaxed">The relaxation limit.</param>
-        /// <returns>The register variables.</returns>
-        private Z3Variable[,] CreateRegisterVariables(Context context, int variables, int relaxed)
-        {
-            Z3Variable[,] registers = new Z3Variable[variables, relaxed];
-            for (int i = 0; i < variables; i++)
-                for (int j = 0; j < relaxed; j++)
-                {
-                    string variableName = string.Format("reg_{0}_{1}", i, j);
-                    registers[i, j] = new Z3Variable(context, variableName);
-                }
-
-            return registers;
-        }
-
-        /// <summary>
-        /// Determines if the given clauses can be satisfiable.
-        /// </summary>
-        /// <param name="clauses">The clauses.</param>
-        /// <param name="context">The Z3 context.</param>
-        /// <returns>The SAT model.</returns>
-        private Model SAT(List<BoolExpr> clauses, Context context)
-        {
-            Solver solver = context.MkSolver();
-            solver.Assert(clauses.ToArray());
-            Status status = solver.Check();
-
-            if (status != Status.SATISFIABLE)
-                return null;
-
-            return solver.Model;
         }
 
         /// <summary>
