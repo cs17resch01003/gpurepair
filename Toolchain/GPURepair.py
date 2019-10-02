4e52198e739b28360eb8746951f130096861a5b6
Gautam Muduganti <cs17resch01003@iith.ac.in>
2019-10-02 16:12:20 +0530

    Adding sat logging.

--- Toolchain/GPURepair.py
+++ Toolchain/GPURepair.py
@@ -466,7 +466,6 @@ class GPURepairInstance(object):
   def getRepairOptions(self, args):
     options = self.getSharedCruncherAndBoogieOptions(args)
 
-    options.append("/efficientSolving:" + "true")
     if args.time_as_csv is not None:
       options.append("/repairLog:" + os.path.dirname(os.path.abspath(__file__)) + os.sep + "repair.log")
 
