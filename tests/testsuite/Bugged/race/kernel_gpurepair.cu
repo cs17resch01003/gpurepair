b6e55932e6cff2b5d2ef2e595686f70977f934a7
cs17resch01003 <cs17resch01003@iith.ac.in>
2020-03-26 11:15:56 +0530

    Adding solutions provided by GPURepair.

--- tests/testsuite/Bugged/race/kernel_gpurepair.cu
+++ tests/testsuite/Bugged/race/kernel_gpurepair.cu
@@ -3,14 +3,14 @@
 
 #include <cuda.h>
 
-__global__ void race (int* __restrict__ A1)
+__global__ void race (int* A)
 {
   int tid = threadIdx.x;
   int bid = blockIdx.x;
 
   int idx = blockDim.x * bid + tid;
-  int temp = 0;
 
-  temp = A1[idx + 1];
-  A1[idx] = temp;
+  int temp = A[idx + 1];
+  __syncthreads();
+  A[idx] = temp;
 }
---
+++ tests/testsuite/CUDASamples/6_Advanced_reduction_reduce3/kernel_gpurepair.cu
@@ -0,0 +1,45 @@
+//pass
+//--gridDim=64 --blockDim=256
+
+template <class T> __global__ void reduce3(T *g_idata, T *g_odata, unsigned int n);
+template __global__ void reduce3<int>(int *g_idata, int *g_odata, unsigned int n);
+
+#include "common.h"
+
+template <class T>
+__global__ void
+reduce3(T *g_idata, T *g_odata, unsigned int n)
+{
+    T *sdata = SharedMemory<T>();
+
+    // perform first level of reduction,
+    // reading from global memory, writing to shared memory
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x*(blockDim.x*2) + threadIdx.x;
+
+    T mySum = (i < n) ? g_idata[i] : 0;
+
+    if (i + blockDim.x < n){
+        mySum += g_idata[i+blockDim.x];
+    }
+
+    sdata[tid] = mySum;
+    // __syncthreads();
+
+    // do reduction in shared mem
+	__syncthreads();
+    for (unsigned int s=blockDim.x/2; s>0; s>>=1)
+    {
+        if (tid < s)
+        {
+            sdata[tid] = mySum = mySum + sdata[tid + s];
+        }
+
+        // __syncthreads();
+    }
+
+    // write result for this block to global mem
+    if (tid == 0){
+     g_odata[blockIdx.x] = sdata[0];
+    }
+}
