aca10d72247e68e7322c76de13c38f32a87a0f17
Gautam Muduganti <cs17resch01003@iith.ac.in>
2020-09-14 21:13:07 +0530

    Removing the repaired versions.

---
+++ tests/solutions/MicroBenchmarks/1-1-If/kernel.cu
@@ -0,0 +1,17 @@
+//pass
+//--blockDim=32 --gridDim=1
+
+#include <cuda.h>
+
+__global__ void test_Prog(int *A, int N) { 
+	const int tid = threadIdx.x;
+	int alpha=0;
+	if(tid%2==0)
+	{
+		alpha=A[tid+2];
+	}
+	if(tid%6==0)
+	{
+		A[tid]=A[tid]+alpha;
+	}
+}
