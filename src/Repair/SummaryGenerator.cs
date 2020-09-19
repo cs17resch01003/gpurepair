96998e21eb86618f093f602db573cdd15b415c36
Gautam Muduganti <cs17resch01003@iith.ac.in>
2020-09-19 20:49:14 +0530

    Generating summary only when there is a change in the weight.

--- src/Repair/SummaryGenerator.cs
+++ src/Repair/SummaryGenerator.cs
@@ -16,6 +16,23 @@
         /// <return>The changes.</return>
         public IEnumerable<string> GenerateSummary(Dictionary<string, bool> assignments, string filename)
         {
+            int repaired_weight = 0;
+            foreach (string barrierName in assignments.Where(x => x.Value == true).Select(x => x.Key))
+            {
+                Barrier barrier = ProgramMetadata.Barriers[barrierName];
+                repaired_weight += barrier.Weight;
+            }
+
+            int source_weight = 0;
+            foreach (Barrier barrier in ProgramMetadata.Barriers.Values)
+            {
+                if (!barrier.Generated)
+                    source_weight += barrier.Weight;
+            }
+
+            Logger.Log($"SourceWeight;{source_weight}");
+            Logger.Log($"RepairedWeight;{repaired_weight}");
+
             List<string> lines = new List<string>();
             List<string> changes = new List<string>();
 
@@ -23,42 +40,45 @@
             int gridLevelBarriers = 0;
             int barriersInsideLoops = 0;
 
-            foreach (string barrierName in assignments.Keys)
+            if (repaired_weight < source_weight)
             {
-                Barrier barrier = ProgramMetadata.Barriers[barrierName];
-                List<Location> locations = ProgramMetadata.Locations[barrier.SourceLocation];
-
-                string location = ToString(locations);
-                if (!barrier.Generated && !assignments[barrierName])
+                foreach (string barrierName in assignments.Keys)
                 {
-                    string message = string.Format(
-                        "Remove the {0}barrier at location {1}.",
-                        barrier.GridLevel ? "grid-level " : string.Empty,
-                        location);
+                    Barrier barrier = ProgramMetadata.Barriers[barrierName];
+                    List<Location> locations = ProgramMetadata.Locations[barrier.SourceLocation];
 
-                    lines.Add(message);
-                    changes.Add(location);
+                    string location = ToString(locations);
+                    if (!barrier.Generated && !assignments[barrierName])
+                    {
+                        string message = string.Format(
+                            "Remove the {0}barrier at location {1}.",
+                            barrier.GridLevel ? "grid-level " : string.Empty,
+                            location);
 
-                    if (locations.Count > 1)
-                        barriersBetweenCalls++;
-                }
-                else if (barrier.Generated && assignments[barrierName])
-                {
-                    string loopMessage =
-                        ProgramMetadata.LoopBarriers.SelectMany(x => x.Value).Any(x => x.Name == barrierName) ?
-                        barrier.LoopDepth == 0 ? " outside the loop" : " inside the loop" : string.Empty;
+                        lines.Add(message);
+                        changes.Add(location);
 
-                    string message = string.Format(
-                        "Add a {0}barrier at location {1}{2}.",
-                        barrier.GridLevel ? "grid-level " : string.Empty,
-                        location, loopMessage);
+                        if (locations.Count > 1)
+                            barriersBetweenCalls++;
+                    }
+                    else if (barrier.Generated && assignments[barrierName])
+                    {
+                        string loopMessage =
+                            ProgramMetadata.LoopBarriers.SelectMany(x => x.Value).Any(x => x.Name == barrierName) ?
+                            barrier.LoopDepth == 0 ? " outside the loop" : " inside the loop" : string.Empty;
 
-                    lines.Add(message);
-                    changes.Add(location);
+                        string message = string.Format(
+                            "Add a {0}barrier at location {1}{2}.",
+                            barrier.GridLevel ? "grid-level " : string.Empty,
+                            location, loopMessage);
 
-                    barriersBetweenCalls += locations.Count > 1 ? 1 : 0;
-                    gridLevelBarriers += barrier.GridLevel ? 1 : 0;
-                    barriersInsideLoops += barrier.LoopDepth > 0 ? 1 : 0;
+                        lines.Add(message);
+                        changes.Add(location);
+
+                        barriersBetweenCalls += locations.Count > 1 ? 1 : 0;
+                        gridLevelBarriers += barrier.GridLevel ? 1 : 0;
+                        barriersInsideLoops += barrier.LoopDepth > 0 ? 1 : 0;
+                    }
                 }
             }
 
