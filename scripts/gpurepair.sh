e2a003ac0f4cc752f44737e596f16e68eefa4bcd
Gautam Muduganti <cs17resch01003@iith.ac.in>
2021-04-20 00:35:46 +0530

    Adding scripts.

---
+++ scripts/gpurepair.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+rm -rf /datadrive/reports
+
+for i in 0 1 2 3
+do
+	cd /datadrive/gpurepair/
+	git reset --hard
+	git clean -fd
+	git pull https://github.com/cs17resch01003/gpurepair.git master
+
+	clear
+	rm -rf Binaries/
+	cd src
+	xbuild /p:Configuration=Release /p:Platform=x86 GPURepair.sln
+
+	clear
+	cd /datadrive/gpurepair/src/Toolchain
+	./grtester.py /datadrive/gpurepair/tests/testsuite --threads=1 --time-as-csv --csv-file=gpurepair-$i.csv --gropt=--detailed-logging 2>&1 | tee gpurepair-run-$i.log
+
+	cd /datadrive/gpurepair/tests/testsuite
+	zip -r gpurepair-snapshot-$i.zip .
+
+	clear
+	mkdir -p /datadrive/reports
+	mv /datadrive/gpurepair/src/Toolchain/gpurepair-$i.csv /datadrive/reports/gpurepair-$i.csv
+	mv /datadrive/gpurepair/src/Toolchain/gpurepair-$i.metrics.csv /datadrive/reports/gpurepair-$i.metrics.csv
+	mv /datadrive/gpurepair/src/Toolchain/gpurepair-run-$i.log /datadrive/reports/gpurepair-run-$i.log
+	mv /datadrive/gpurepair/tests/testsuite/gpurepair-snapshot-$i.zip /datadrive/reports/gpurepair-snapshot-$i.zip
+done
